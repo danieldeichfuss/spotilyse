4056d241c2210f1926a9497c4f42d09ad94afa75
danieldeichfuss <d.deichfuss@gmail.com>
2018-05-26 17:56:08 +0200

    Create variables for favourites

--- src/Components/favourite-songs.jsx
+++ src/Components/favourite-songs.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 export default function FavouriteSongs(props) {
+  let favouriteSong = props.favouriteSong || null;
+  let favouriteArtist = props.favouriteArtist || null;
+  console.log(favouriteSong);
+  console.log(favouriteArtist);
+
   return (
     <div>
       <h2>What your favourite songs say about you</h2>
-      <span>
-        Your favourite song is:{" "}
-        {props.favouriteSong && props.favouriteSong.name}
-      </span>
-      <span>
-        Your favourite artist is:{" "}
-        {props.favouriteArtist && props.favouriteArtist.name}
-      </span>
+      <div>Your favourite song is: {favouriteSong && favouriteSong.name}</div>
+      <div>
+        Your favourite artist is: {favouriteArtist && favouriteArtist.name}
+      </div>
     </div>
   );
 }
