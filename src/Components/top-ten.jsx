56d804b78afb647c5110bec87279ec01d88cede1
danieldeichfuss <d.deichfuss@gmail.com>
2018-05-26 18:00:12 +0200

    Add keys for lists

--- src/Components/top-ten.jsx
+++ src/Components/top-ten.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import Discover from "./discover";
 
 export default function TopTen(props) {
   const tracksToRender =
-    props.tracks && props.tracks.map(track => <li>{track.name}</li>);
+    props.tracks &&
+    props.tracks.map((track, i) => <li key={i.toString()}>{track.name}</li>);
   const artistsToRender =
-    props.artists && props.artists.map(artist => <li>{artist.name}</li>);
+    props.artists &&
+    props.artists.map((artist, i) => <li key={i.toString()}>{artist.name}</li>);
   return (
     <div>
       <h2>Your Top 10</h2>
       <h3>Top 10 Tracks</h3>
-      {tracksToRender}
+      <ul>{tracksToRender}</ul>
       <h3>Top 10 Artists</h3>
-      {artistsToRender}
+      <ul>{artistsToRender}</ul>
     </div>
   );
 }
