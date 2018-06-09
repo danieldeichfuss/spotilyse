b809784d1c2e2e3b2697d8e30ece191116bd96e2
danieldeichfuss <d.deichfuss@gmail.com>
2018-06-09 23:19:32 +0200

    Fill PreLogin and PostLogin

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import eyes from "../img/eyes.svg";
 
 export default function Login(props) {
   return (
     <div className="Login">
+      <p className="Login__intro">Take a deep dive into your soul ...</p>
+      <img className="Login__eyes" src={eyes} alt="eyes" />
+      <p className="Login__intro">... I mean musicl Soul</p>
       <button className="Login__button" onClick={props.initLogin}>
-        Login
+        Login with Spotify
       </button>
     </div>
   );
--- src/Components/user.jsx
+++ src/Components/user.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-export default function User(props) {
+export default function User({ user, initExpoloration }) {
   return (
     <div className="User">
-      <img className="User__avatar" src={props.user.imageUrl} alt="" />
-      <h2 className="User__name">{props.user.name}</h2>
-      <span className="User__product">{props.user.product}</span>
+      <img className="User__avatar" src={user.imageUrl} alt="" />
+      <p className="User__intro">
+        Hi {user.name},
+        <br />
+        welcome to Spotilyse! Here you can expolore your musical personality and discover your favourite artists.
+      </p>
+      <button className="Login__button" onClick={initExpoloration}>
+        Start exploring
+      </button>
     </div>
   );
 }
