cce11efb67f7d92c4b20ae63b14886a520de5b07
danieldeichfuss <d.deichfuss@gmail.com>
2018-06-12 13:31:02 +0200

    Add emojis

--- src/Components/audio-feature.js
+++ src/Components/audio-feature.js
@@ -1,16 +1,82 @@
 import React from "react";
+import guitar from "../img/guitar.svg";
+import robot from "../img/robot.svg";
+import dancer from "../img/dancer-male.svg";
+import stone from "../img/stone.svg";
+import thunder from "../img/thunder.svg";
+import sunglasses from "../img/sunglasses.svg";
+import mikrophones from "../img/mikrophone.svg";
+import headphones from "../img/headphones.svg";
+import cloud from "../img/cloud.svg";
+import ear from "../img/ear.svg";
+import bunny from "../img/bunny.svg";
+import snail from "../img/snail.svg";
+import sad from "../img/sad.svg";
+import sun from "../img/sun.svg";
 
 const AudioFeature = ({ audioFeature, featureValue }) => {
+  const message =
+    featureValue > audioFeature.breakPoint
+      ? audioFeature.messages[1]
+      : audioFeature.messages[0];
+  let img = null;
+
+  switch (message.img) {
+    case "guitar":
+      img = guitar;
+      break;
+    case "robot":
+      img = robot;
+      break;
+    case "dancer":
+      img = dancer;
+      break;
+    case "stone":
+      img = stone;
+      break;
+    case "thunder":
+      img = thunder;
+      break;
+    case "sunglasses":
+      img = sunglasses;
+      break;
+    case "mikrophones":
+      img = mikrophones;
+      break;
+    case "headphones":
+      img = headphones;
+      break;
+    case "ear":
+      img = ear;
+      break;
+    case "cloud":
+      img = cloud;
+      break;
+    case "bunny":
+      img = bunny;
+      break;
+    case "snail":
+      img = snail;
+      break;
+    case "sun":
+      img = sun;
+      break;
+    case "sad":
+      img = sad;
+      break;
+    default:
+      img = null;
+  }
+
+  console.log(audioFeature.name);
+
   return (
     <div className="AudioFeatures__item">
       <div className="AudioFeatures__value">
         {audioFeature.name}: {featureValue}
       </div>
-      <div className="AudioFeatures__result">
-        {featureValue > audioFeature.breakPoint
-          ? audioFeature.messages[1].text
-          : audioFeature.messages[0].text}
-      </div>
+      <div className="AudioFeatures__result">{message.text}</div>
+      <img src={img} alt="emoji" />
     </div>
   );
 };
