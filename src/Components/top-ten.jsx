b39d1dae99b0582e159c9350761daab8301ef31c
danieldeichfuss <d.deichfuss@gmail.com>
2018-06-16 16:55:14 +0200

    Add list style

--- src/Components/top-ten.jsx
+++ src/Components/top-ten.jsx
@@ -3,17 +3,25 @@ import React from "react";
 export default function TopTen(props) {
   const tracksToRender =
     props.tracks &&
-    props.tracks.map((track, i) => <li key={i.toString()}>{track.name}</li>);
+    props.tracks.map((track, i) => (
+      <li className="TopTen__item" key={i.toString()}>
+        <span>{track.name}</span>
+      </li>
+    ));
   const artistsToRender =
     props.artists &&
-    props.artists.map((artist, i) => <li key={i.toString()}>{artist.name}</li>);
+    props.artists.map((artist, i) => (
+      <li className="TopTen__item" key={i.toString()}>
+        <span>{artist.name}</span>
+      </li>
+    ));
   return (
     <div className="TopTen">
       <h2 className="TopTen__title">Your Top 10</h2>
       <h3 className="TopTen__sub-title">Top 10 Tracks</h3>
-      <ul className="TopTen__list">{tracksToRender}</ul>
+      <ol className="TopTen__list">{tracksToRender}</ol>
       <h3 className="TopTen__sub-title">Top 10 Artists</h3>
-      <ul className="TopTen__list">{artistsToRender}</ul>
+      <ol className="TopTen__list">{artistsToRender}</ol>
     </div>
   );
 }
