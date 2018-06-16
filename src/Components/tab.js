53c9b4d3143f9163f33b1fe681ca95394576a4b9
danieldeichfuss <d.deichfuss@gmail.com>
2018-06-16 09:03:19 +0200

    Add better hover animation for tabs

--- src/Components/tab.js
+++ src/Components/tab.js
@@ -2,14 +2,16 @@ import React from "react";
 
 const Tab = props => {
   return (
-    <li
-      className={`Tab ${props.isActive ? "active" : ""}`}
-      onClick={event => {
-        event.preventDefault();
-        props.onClick(props.tabIndex);
-      }}
-    >
-      {props.tabName}
+    <li className="Tab">
+      <a
+        className={`Tab__link ${props.isActive ? "active" : ""}`}
+        onClick={event => {
+          event.preventDefault();
+          props.onClick(props.tabIndex);
+        }}
+      >
+        {props.tabName}
+      </a>
     </li>
   );
 };
