cb9769b348df3ff75d09d0891f8e3983fbe414e0
danieldeichfuss <d.deichfuss@gmail.com>
2018-06-21 12:47:10 +0200

    Animate eyes

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import eyes from "../img/eyes.svg";
+import Eyes from "../img/eyes";
 
 export default function Login(props) {
   return (
     <div className="Login">
       <p className="Login__intro">Take a deep dive into your soul ...</p>
-      <img className="Login__eyes" src={eyes} alt="eyes" />
+      <Eyes />
       <p className="Login__intro">... I mean musicl Soul</p>
       <button className="Login__button" onClick={props.initLogin}>
         Login with Spotify
---
+++ src/img/eyes.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+export default () => {
+  return (
+    <svg className="eyes" viewBox="0 0 47.5 47.5" id="svg2">
+      <defs id="defs6">
+        <clipPath id="clipPath16" clipPathUnits="userSpaceOnUse">
+          <path id="path18" d="M 0,38 38,38 38,0 0,0 0,38 Z" />
+        </clipPath>
+      </defs>
+      <g transform="matrix(1.25,0,0,-1.25,0,47.5)" id="g10">
+        <g id="g12">
+          <g clip-path="url(#clipPath16)" id="g14">
+            <g transform="translate(18,19)" id="g20">
+              <path
+                className="eye-ball eye-ball-left"
+                id="path22"
+                style={{
+                  fill: "#e1e8ed",
+                  fillOpacity: 1,
+                  fillRule: "nonzero",
+                  stroke: "none"
+                }}
+                d="m 0,0 c 0,-5.522 -3.806,-10 -8.5,-10 -4.694,0 -8.5,4.478 -8.5,10 0,5.523 3.806,10 8.5,10 C -3.806,10 0,5.523 0,0"
+              />
+            </g>
+            <g transform="translate(37,19)" id="g24">
+              <path
+                className="eye-ball eye-ball-right"
+                id="path26"
+                style={{
+                  fill: "#e1e8ed",
+                  fillOpacity: 1,
+                  fillRule: "nonzero",
+                  stroke: "none"
+                }}
+                d="m 0,0 c 0,-5.522 -3.806,-10 -8.5,-10 -4.694,0 -8.5,4.478 -8.5,10 0,5.523 3.806,10 8.5,10 C -3.806,10 0,5.523 0,0"
+              />
+            </g>
+            <g transform="translate(9,15)" id="g28">
+              <path
+                className="eye-dot eye-dot-left"
+                id="path30"
+                style={{
+                  fill: "#292f33",
+                  fillOpacity: 1,
+                  fillRule: "nonzero",
+                  stroke: "none"
+                }}
+                d="m 0,0 c 0,-1.657 -1.343,-3 -3,-3 -1.657,0 -3,1.343 -3,3 0,1.657 1.343,3 3,3 1.657,0 3,-1.343 3,-3"
+              />
+            </g>
+            <g transform="translate(28,15)" id="g32">
+              <path
+                className="eye-dot eye-dot-left"
+                id="path34"
+                style={{
+                  fill: "#292f33",
+                  fillOpacity: 1,
+                  fillRule: "nonzero",
+                  stroke: "none"
+                }}
+                d="m 0,0 c 0,-1.657 -1.344,-3 -3,-3 -1.656,0 -3,1.343 -3,3 0,1.657 1.344,3 3,3 1.656,0 3,-1.343 3,-3"
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
