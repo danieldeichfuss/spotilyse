a981627cbdaf256e88f7203e980dac1961ec5c42
danieldeichfuss <d.deichfuss@gmail.com>
2018-06-24 19:28:09 +0200

    Update Readme

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,7 +7,7 @@ export default function Login({ initLogin }) {
     <div className="Login">
       <p className="Login__intro">Take a deep dive into your soul ...</p>
       <Eyes />
-      <p className="Login__intro">... I mean musicl Soul</p>
+      <p className="Login__intro">... I mean musical Soul</p>
       <Button callback={initLogin} text="Login with Spotify" />
     </div>
   );
