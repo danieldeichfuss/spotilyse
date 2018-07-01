7ee40e6e12d8e3cb6009cbfb6b85615ea77d2442
danieldeichfuss <d.deichfuss@gmail.com>
2018-07-01 12:03:07 +0200

    Add new logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Logo from "../img/Logo";
 
 function Header() {
   return (
     <header className="Header">
-      <h1 className="Header__title">Spotilyse</h1>
+      {/* <h1 className="Header__title">Spotilyse</h1> */}
+      <Logo />
       <p className="Header__intro">Your Spotify personality dashboard</p>
     </header>
   );
