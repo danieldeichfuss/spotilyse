c57ad16c07c8b1390ca29ae4976b727bf5a25f26
danieldeichfuss <d.deichfuss@gmail.com>
2018-07-11 17:18:22 +0200

    Remove comment

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,6 @@ import Logo from "../img/Logo";
 function Header() {
   return (
     <header className="Header">
-      {/* <h1 className="Header__title">Spotilyse</h1> */}
       <Logo />
       <p className="Header__intro">Your Spotify personality dashboard</p>
     </header>
